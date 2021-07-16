bdecb6c16b6e25404e9dbdd679333d4f6b9d0557
Petronilha <danielpetronilhajobs@gmail.com>
2021-07-16 00:04:25 -0300

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3333
 const indexRouter = require('./routes/index')
 const tabuadaRouter = require( './routes/tabuada')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 //view engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
